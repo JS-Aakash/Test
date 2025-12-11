a94d181697055a6c219bc1acd9ef9e6213aa15f0
JS-Aakash <jsaakash22@gmail.com>
2025-12-11 11:31:01 +0530

    fix: resolve issue #1

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+/**
+ * Returns the sum of two numbers.
+ *
+ * @param {number} a The first number.
+ * @param {number} b The second number.
+ * @returns {number} The sum of a and b.
+ */
 function add(a, b) {
-    return a - b;
+    return a + b;
 }
 
+/**
+ * Returns a greeting message for the given name.
+ *
+ * @param {string} name The name to greet.
+ * @returns {string} A greeting message.
+ */
 function greet(name) {
     return "Hello " + name;
 }
 
+// Export the functions for use in other modules.
 module.exports = { add, greet };
